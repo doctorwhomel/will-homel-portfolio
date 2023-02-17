b31cae63315cced3df09e115414d01d451c35d4e
WHOMEL <williamhomel@gmail.com>
2023-02-17 11:24:40 -0800

    net

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./App.css";
 import Header from "./components/Header";
