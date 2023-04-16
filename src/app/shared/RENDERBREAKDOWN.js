fa3736422fb8b48435c5dd4f37e944ae26738f3d
WHOMEL <williamhomel@gmail.com>
2023-04-16 13:20:35 -0700

    renderBreakdown

---
+++ src/app/shared/RENDERBREAKDOWN.js
@@ -0,0 +1,32 @@
+import officeRendered from "../assets/img/nightOfficev4.jpg";
+import officeMaterials from "../assets/img/officeMaterials.jpg";
+import officeSolid from "../assets/img/officeMesh.jpg";
+import officeWireframe from "../assets/img/officeWireframe.jpg";
+
+export const RENDERBREAKDOWN = [
+  {
+    albumId: 5,
+    id: 1,
+    title: "Wireframe",
+    url: officeWireframe,
+    thumbnailUrl: officeWireframe,
+  },
+  {
+    albumId: 5,
+    title: "Solid",
+    url: officeSolid,
+    thumbnailUrl: officeSolid,
+  },
+  {
+    albumId: 5,
+    title: "Materials",
+    url: officeMaterials,
+    thumbnailUrl: officeMaterials,
+  },
+  {
+    albumId: 5,
+    title: "Rendered",
+    url: officeRendered,
+    thumbnailUrl: officeRendered,
+  },
+];
--- src/pages/GamesPage.js
+++ src/pages/GamesPage.js
@@ -1,10 +1,13 @@
 import ReactstrapImageGallery from "../components/ReactstrapImageGallery";
+import { RENDERBREAKDOWN } from "../app/shared/RENDERBREAKDOWN";
 import { RENDERS } from "../app/shared/RENDERS";
 import { ASSETS } from "../app/shared/ASSETS";
 
 const GamesPage = () => {
   return (
     <div>
+      <h5>Render Breakdown</h5>
+      <ReactstrapImageGallery images={RENDERBREAKDOWN} limit={12} />
       <h5>Renders</h5>
       <ReactstrapImageGallery images={RENDERS} limit={12} />
       <h5>Assets</h5>
